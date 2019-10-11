6b47673ca046b677434eb300607bd2bf1bcfb268
leonardo <leonardo@caffelli.com>
2019-10-10 21:58:31 -0500

    Updated profile

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -81,10 +81,7 @@ const Head = props => {
     <Helmet>
       <html lang={config.siteLanguage} />
       <title>{title}</title>
-      <link rel="apple-touch-icon" href="/favicons/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-      <meta name="gatsby-starter" content="Gatsby Starter Portfolio Cara" />
+      <meta name="gatsby-starter" content="Leonardo's hood" />
       <link rel="shortcut icon" href="favicon.ico" />
       <meta name="msapplication-TileColor" content={config.backgroundColor} />
       <meta name="msapplication-config" content="browserconfig.xml" />
